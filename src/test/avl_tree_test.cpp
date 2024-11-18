1c2b8f25668c2bf7ec37b996005df0aabd728f06
sewon <dltpdnjs2000@gmail.com>
2024-11-18 16:03:13 +0900

    feat: Add AVLTreeTest Fixture for test

--- src/test/avl_tree_test.cpp
+++ src/test/avl_tree_test.cpp
@@ -27,6 +27,37 @@
 //
 //     2024.11 GUGONG2
 
-#include "../avl_tree.h"
-
 #include <gtest/gtest.h>
+#include "../avl_tree.cpp"
+
+// AVLTreeTest 클래스는 테스트를 위한 공통된 설정을 제공하는 Fixture
+class AVLTreeTest : public ::testing::Test {
+protected:
+  Node<int>* left;
+  Node<int>* right;
+  Node<int>* root;
+  AVLTree<int> tree;
+
+  // 각 테스트 실행 전 호출되어 트리 설정
+  void SetUp() override {
+    //      10
+    //     /  \
+    //    6   12
+    left  = new Node<int>{6, nullptr, nullptr, 1, 1};
+    right = new Node<int>{12, nullptr, nullptr, 1, 1};
+    root  = new Node<int>{10, left, right, 2, 3};
+    tree  = AVLTree<int>(root);
+  }
+
+  // 각 테스트 종료 후 호출되어 메모리를 해제
+  void TearDown() override {
+    delete left;
+    delete right;
+    delete root;
+  }
+};
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

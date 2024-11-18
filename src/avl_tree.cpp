1c422b2e7965a6d5a5ceb1833a251b5e9fc8171c
leejaehun <ldyjka@gmail.com>
2024-11-18 12:34:19 +0900

    feat: Implement Insert function for AVL Tree

--- src/avl_tree.cpp
+++ src/avl_tree.cpp
@@ -94,3 +94,89 @@ Node<T>* AVLTree<T>::FindNode(Node<T>* node, T target) {
   }
   return nullptr;  // 찾지 못한 경우 nullptr 반환
 }
+template <typename T>
+T max(T a, T b) {
+  return (a > b) a : b;
+}
+
+template <typename T>
+void updateHeight(Node<T>* node) {
+  int leftHeight  = GetHeight(node->left_);
+  leftHeight      = (leftHeight == -1) ? 0 : leftHeight;
+  int rightHeight = GetHeight(node->right_);
+  rightHeight     = (rightHeight == -1) ? 0 : rightHeight;
+  // height update
+  node->height_ = 1 + max(leftHeight, rightHeight);
+}
+
+template <typename T>
+Node<T>* rightRotate(Node<T>* node) {
+  Node<T>* leftChild      = node->left_;
+  Node<T>* leftRightChild = leftChild->right_;
+
+  leftChild->right_ = node;
+  node->left_       = leftRightChild;
+
+  updateHeight(node);
+  updateHeight(leftChild);
+
+  return leftChild;
+}
+
+template <typename T>
+Node<T>* leftRotate(Node<T>* node) {
+  Node<T>* rightChild     = node->right_;
+  Node<T>* rightLeftChild = rightChild->left_;
+
+  rightChild->left = node;
+  node->right_     = rightLeftChild;
+
+  updateHeight(node);
+  updateHeight(rightChild);
+
+  return rightChild;
+}
+
+template <typename T>
+Node<T>* InsertNode(Node<T>* node, T target) {
+  // Insert 특정상 node에는 root 고정. target 에 Insert Node 정보가 들어감.
+  if (node == nullptr) {
+    // TODO
+    // Node 생성자 만들고 new Node return;
+  }
+  if (target < node->key_) {
+    node->left = InsertNode(node->left_, target);
+  } else if (target > node->key_) {
+    node->right_ = InsertNode(node->right_, target)
+  } else {
+    // TODO
+    // 중복된 값이 insert 되는 경우
+  }
+
+  updateHeight(node);
+
+  // get balance factor in specific node
+  int bf = leftHeight - rightHeight;
+
+  if (bf > 1 && target < node->left_->key_) {
+    return rightRotate(node);
+  }
+  if (bf < -1 && target > node->right_->key_) {
+    return leftRotate(node);
+  }
+  if (bf > 1 && target > node->left_->key_) {
+    node->left_ = leftRotate(node->left_);
+    return rightRotate(node);
+  }
+  if (bf < -1 && target < node->right_->key_) {
+    node->right_ = rightRotate(node->right_);
+    return leftRotate(node);
+  }
+  return node;
+}
+
+template <typename T>
+int AVLTree<T>::insert(T target) {
+  root_ = InsertNode(root_, target);
+  return GetDepth(root_, target) + FindNode(root_, target)->height_;
+}

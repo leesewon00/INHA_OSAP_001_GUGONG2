9dad8fa9c7361902907d1ff79d3cf655377cf368
dlwogns <61345151+dlwogns@users.noreply.github.com>
2024-11-18 22:11:32 +0900

    Tree Interface 상속을 통한 AVLTree 구체 클래스 구현 (#12)
    * feat: Add constructor to initialize Node
    
    * chore: convert InsertNode to Insert
    
    * feat: Implement Insert function for AVL Tree
    
    * refactor: Modify method, variables name according to convention
    
    * chore: Add insert-based methods to AVLTree class
    
    * fix: Conting balancing factor without local variable occurs error. make variables to count bf
    
    * comment: Add annotation associated with Insert function
    
    * feat: Construct Tree Interface and make AVLtree override it

---
+++ src/tree.h
@@ -0,0 +1,21 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "node.h"
+
+template <typename T>
+class Tree {
+public:
+  virtual bool IsEmpty(Node<T>* node)                = 0;
+  virtual int GetSize(Node<T>* node)                 = 0;
+  virtual int GetHeight(Node<T>* node)               = 0;
+  virtual Node<T>* FindNode(Node<T>* node, T target) = 0;
+  virtual int Insert(T target)                       = 0;
+  virtual void GetAncestor(Node<T>* node, T target)  = 0;
+  virtual void GetAverage(Node<T>* node)             = 0;
+  virtual void EraseNode(Node<T>* node, T target)    = 0;
+  virtual void GetRank(Node<T>* node, T target)      = 0;
+  virtual ~Tree()                                    = default;
+};
+
+#endif  // TREE_H

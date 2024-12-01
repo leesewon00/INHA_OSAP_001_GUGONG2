2c3c79ab8d0a6387f22bf32cfe0e772279110c5d
dlwogns <61345151+dlwogns@users.noreply.github.com>
2024-12-01 20:55:55 +0900

    EraseNode 함수 구현, Insert시 Node->size 변수 갱신 로직 작성 (#20)
    * feat: Implement EraseNode function
    
    * feat: Add size increment logic in Insert Function
    
    * fix: Convert Retun value of EraseNode function to Node pointer

--- src/tree.h
+++ src/tree.h
@@ -6,17 +6,17 @@
 template <typename T>
 class Tree {
 public:
-  virtual bool IsEmpty(Node<T>* node)                = 0;
-  virtual int GetSize(Node<T>* node)                 = 0;
-  virtual int GetHeight(Node<T>* node)               = 0;
-  virtual Node<T>* FindNode(Node<T>* node, T target) = 0;
-  virtual int Insert(T target)                       = 0;
-  virtual void GetAncestor(Node<T>* node, T target)  = 0;
-  virtual void GetAverage(Node<T>* node)             = 0;
-  virtual void EraseNode(Node<T>* node, T target)    = 0;
-  virtual void GetRank(Node<T>* node, T target)      = 0;
-  virtual Node<T>* getRoot()                         = 0;
-  virtual ~Tree()                                    = default;
+  virtual bool IsEmpty(Node<T>* node)                 = 0;
+  virtual int GetSize(Node<T>* node)                  = 0;
+  virtual int GetHeight(Node<T>* node)                = 0;
+  virtual Node<T>* FindNode(Node<T>* node, T target)  = 0;
+  virtual int Insert(T target)                        = 0;
+  virtual void GetAncestor(Node<T>* node, T target)   = 0;
+  virtual void GetAverage(Node<T>* node)              = 0;
+  virtual Node<T>* EraseNode(Node<T>* node, T target) = 0;
+  virtual void GetRank(Node<T>* node, T target)       = 0;
+  virtual Node<T>* getRoot()                          = 0;
+  virtual ~Tree()                                     = default;
 };
 
 #endif  // TREE_H

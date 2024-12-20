88bf2661864fdf5e88d6197b53c324f16d8dbc07
leejaehun <ldyjka@gmail.com>
2024-12-20 20:03:02 +0900

    comment: Add license in main_sub.cpp

--- src/main_sub.cpp
+++ src/main_sub.cpp
@@ -1,3 +1,32 @@
+// MIT License
+
+// Copyright(c) 2024 GUGONG2
+
+//     Permission is hereby granted,
+//     free of charge,
+//     to any person obtaining a copy of this software and associated
+//     documentation files(the "Software"), to deal in the Software without
+//     restriction, including without limitation the rights to use, copy,
+//     modify, merge, publish
+//     ,
+//     distribute, sublicense, and / or sell copies of the Software,
+//     and to permit persons to whom the Software is furnished to do so,
+//     subject to the following conditions :
+
+//     The above copyright notice and this permission notice shall be included
+//     in all copies or substantial portions of the Software.
+
+//     THE SOFTWARE IS PROVIDED "AS IS",
+//     WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//     INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL
+//     THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//     OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//     OTHER DEALINGS IN THE SOFTWARE.
+//
+//     2024.11 GUGONG2
+
 #include <iostream>
 #include <string>
 #include <utility>
@@ -357,9 +386,7 @@ Node<T>* AVLTree<T>::EraseNode(Node<T>* node, T target) {
   if (node == nullptr) {
     return node;
   }
-  // int lh        = node->left_ ? node->left_->height_ : 0;
-  // int rh        = node->right_ ? node->right_->height_ : 0;
-  // node->height_ = 1 + max(lh, rh);
+
   UpdateHeight(node);
 
   int bf = getBf(node);
